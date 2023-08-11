8491c9922f0a8315f2ea78451e8ec7b104e9c890
MiloTruck <milotruckctf@gmail.com>
2023-08-12 00:01:45 +0800

    Minor change

--- tests/submission.sol
+++ tests/submission.sol
@@ -4,9 +4,3 @@ pragma solidity 0.8.19;
 import {FixedPointMathLibTest} from "./FixedPointMathLibTest.sol";
 import {SignedWadMathTest} from "./SignedWadMathTest.sol";
 import {ERC20Test} from "./ERC20Test.sol";
-
-/*
-Fuzz with the following command:
-
-./medusa fuzz --target submission.sol --deployment-order FixedPointMathLibTest,SignedWadMathTest,ERC20Test 
-*/
